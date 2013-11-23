98a18734f87a6d9d36adf555af9b4f74585f4c2f
viniciusrdacosta <viniciusrdacosta@gmail.com>
2013-11-23 11:01:39 -0200

    Criacao pasta docs

--- src/main/java/br/edu/uniritter/liquid/democracy/validator/CustomMatchers.java
+++ src/main/java/br/edu/uniritter/liquid/democracy/validator/CustomMatchers.java
@@ -1,5 +1,5 @@
 /***
- * Copyright (c) 2013 SportShare - www.sportshare.me/opensource
+ * Copyright (c) 2013 Liquid Democracy
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); 
