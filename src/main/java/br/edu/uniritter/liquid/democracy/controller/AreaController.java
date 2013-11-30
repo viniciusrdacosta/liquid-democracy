43e97e1daa1b84065b60d38293e193d3cf015397
Diego Gualtieri <diego@diego.blog.br>
2013-11-30 10:19:19 -0200

    Ajuste AreaControle

--- src/main/java/br/edu/uniritter/liquid/democracy/controller/AreaController.java
+++ src/main/java/br/edu/uniritter/liquid/democracy/controller/AreaController.java
@@ -1,12 +1,19 @@
 package br.edu.uniritter.liquid.democracy.controller;
 
+import static br.edu.uniritter.liquid.democracy.validator.CustomMatchers.notNull;
+import static org.hamcrest.Matchers.is;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.validator.Validations;
 import br.edu.uniritter.liquid.democracy.annotation.Public;
+import br.edu.uniritter.liquid.democracy.model.User;
+import br.edu.uniritter.liquid.democracy.service.AreaService;
 import br.edu.uniritter.liquid.democracy.service.SecurityService;
 import br.edu.uniritter.liquid.democracy.session.UserInfo;
+
+
 
 
 @Resource
@@ -14,8 +21,15 @@ public class AreaController {
 	
 	private Result result;
 	private UserInfo info;
-	private AreasService service;
+	private AreaService service;
 	private final Validator validator;
+	
+	public AreaController(Result result, UserInfo info, AreaService service, Validator validator) {
+		this.result = result;
+		this.info = info;
+		this.service = service;
+		this.validator = validator;
+	}
 	
 	@Public
 	@Get("/areas")
@@ -26,5 +40,13 @@ public class AreaController {
 	@Get("/edit")
 	public void add() {}
 	
-	
+	private void validate(final User user){
+		validator.checking(new Validations() {
+			{
+				that(user, is(notNull()), "error", "error.login");
+			}
+		});
+
+		validator.onErrorUsePageOf(SecurityController.class).login();
+	}
 }
--- src/main/java/br/edu/uniritter/liquid/democracy/dao/AreaDao.java
+++ src/main/java/br/edu/uniritter/liquid/democracy/dao/AreaDao.java
@@ -11,13 +11,12 @@ public class AreaDao extends AbstractDao<Area> implements AreaRepository{
 
 	protected AreaDao(EntityManager entityManager) {
 		super(entityManager);
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	@Override
 	public List<Area> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return super.findAll();
 	}
 	
 	
