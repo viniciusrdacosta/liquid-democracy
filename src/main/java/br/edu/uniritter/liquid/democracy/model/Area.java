02dfe9b350da10a59af2b552fdc74fd7d0378685
Cristian Pietro <cristian.pietro@gmail.com>
2013-12-13 19:47:10 -0200

    Merge.

--- src/main/java/br/edu/uniritter/liquid/democracy/model/Area.java
+++ src/main/java/br/edu/uniritter/liquid/democracy/model/Area.java
@@ -1,61 +1,17 @@
 package br.edu.uniritter.liquid.democracy.model;
 
 import java.io.Serializable;
-<<<<<<< HEAD
-import java.util.Date;
-
-//@Entity
-//@Table(name = "candidate")
-public class Area implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	// @Column(name = "number")
-	private Candidate voter;
-	private Project project;
-	private Date date;
-	private Long weight;
-
-	public Candidate getVoter() {
-		return voter;
-	}
-
-	public void setVoter(Candidate voter) {
-		this.voter = voter;
-	}
-
-	public Project getProject() {
-		return project;
-	}
-
-	public void setProject(Project project) {
-		this.project = project;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Long getWeight() {
-		return weight;
-	}
-
-	public void setWeight(Long weight) {
-		this.weight = weight;
-=======
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
-
+@Entity
 @Table(name = "area")
 public class Area extends BaseEntity implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+
+	private static final long serialVersionUID = 1431969740928027523L;
 
 	@Column(name = "name")
 	String name;
@@ -77,7 +33,6 @@ public class Area extends BaseEntity implements Serializable {
 
 	public void setDescription(String description) {
 		this.description = description;
->>>>>>> 43e97e1daa1b84065b60d38293e193d3cf015397
 	}
 
 }
