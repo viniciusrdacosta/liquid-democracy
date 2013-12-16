c2d910ca1122fc158d391177e0d2105631d3f42a
viniciusrdacosta <viniciusrdacosta@gmail.com>
2013-12-16 21:05:50 -0200

    Remocao arquivos

--- src/main/java/br/edu/uniritter/liquid/democracy/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.uniritter.liquid.democracy.repository;
-
-import br.edu.uniritter.liquid.democracy.model.User;
-
-public interface UserRepository  {
-	
-	User update(User entity);
-
-}
